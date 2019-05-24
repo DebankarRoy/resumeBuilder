e4fbe41c72625a99c0927096f45ec0f6069b02a7
debankar <rdebankar@gmail.com>
2019-05-24 16:38:11 +0530

    fisrt approach

---
+++ home.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+		<link rel="stylesheet" href="css/home.css"/>
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
+		<title>Homepage</title>
+	</head>
+		<div class="row upper">
+			<div class="col logo"></div>
+			<div class="col mid"></div>
+			<div class="col user"></div>
+		</div>
+		<div class="row lower">
+			<div class="row slide">
+				<img src="img/music-control-settings-button.png" class="attr pref">
+				<img src="img/user.png" class="attr prof">
+				<img src="img/mortarboard.png" class="attr edu">
+				<img src="img/test.png" class="attr work">
+				<img src="img/artificial-intelligence.png" class="attr skills">
+				<img src="img/folder1.png" class="attr worksample">
+
+			</div>
+			<div class="row data">
+				<div class="row info">
+					<div class="datatitle">Education</div>
+				</div>
+				<div class="row dialouge"><h4>Tell us bit about your education</h4></div>
+				<div class="col padding"></div>
+				<div class="col userinfo">
+					<div class="row discipline" data-toggle="modal" data-target="#collegeModal">Graduation
+						<i class="fa fa-plus education-fa-plus fa-x" aria-hidden="true"></i>
+					</div>
+					<div class="row discipline" data-toggle="modal" data-target="#collegeModal">XII(Higer Secondary)
+						<i class="fa fa-plus education-fa-plus fa-x" aria-hidden="true"></i>
+					</div>
+					<div class="row discipline" data-toggle="modal" data-target="#collegeModal">X(Secondary)
+						<i class="fa fa-plus education-fa-plus fa-x" aria-hidden="true"></i>
+					</div>
+					<div class="row discipline" data-toggle="modal" data-target="#collegeModal">Post Graduation
+						<i class="fa fa-plus education-fa-plus fa-x" aria-hidden="true"></i>
+					</div>
+					<div class="row discipline" data-toggle="modal" data-target="#collegeModal">Diploma
+						<i class="fa fa-plus education-fa-plus fa-x" aria-hidden="true"></i>
+					</div>
+					<div class="row discipline" data-toggle="modal" data-target="#collegeModal">PhD
+						<i class="fa fa-plus education-fa-plus fa-x" aria-hidden="true"></i>
+					</div>
+					<div class="button-container">
+						<a href="" class="previous">PREVIOUS</a>
+						<a href="" class="next-container"><button class="next">Next</button></a>
+					</div>	
+				</div>
+				<div class="col padding"></div>
+				
+
+			</div>
+		</div>
+		<a href="https://www.flaticon.com">for icons</a>
+	<body>
+
+	</body>
+</html>
+
